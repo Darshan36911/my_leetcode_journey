9498e3ea76e23ca903911eb68365f8b9e70efdc2
Darshan <213953128+Darshan36911@users.noreply.github.com>
2026-09-07 22:07:20 +0530

    Time: 11 ms (61.98%), Space: 19.3 MB (46.35%) - LeetHub

---
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def distinctSubseqII(self, s):
+        MOD = 10 ** 9 + 7
+        n = len(s)
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        last = {}
+        for i, c in enumerate(s, 1):
+            dp[i] = (2 * dp[i - 1]) % MOD
+            if c in last:
+                dp[i] = (dp[i] - dp[last[c] - 1]) % MOD
+            last[c] = i
+        return (dp[n] - 1) % MOD
